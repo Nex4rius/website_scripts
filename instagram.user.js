8b53527ab7dccce12e14f760ab4fb00f6a63b3aa
Nexarius <github@nexarius.xyz>
2022-07-29 23:28:43 +0200

    Update instagram.user.js

--- instagram.user.js
+++ instagram.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @version     2
-// @author      Nexarius
-// @homepage    https://github.com/Nex4rius/website_scripts
 // @name        instagram
+// @author      Nexarius
+// @version     3
+// @homepage    https://github.com/Nex4rius/website_scripts
 // @description remove obstruction for the html image element -> rightclick works now normally again -> fuck you facebook
 
 // @include    *instagram.com*
