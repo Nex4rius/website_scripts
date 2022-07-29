5c3026494131b4e3caef2fa079f211a00496ff8d
Nexarius <github@nexarius.xyz>
2022-07-29 23:08:49 +0200

    Create tiktok_rechtsklick_steuerung.user.js

---
+++ tiktok_rechtsklick_steuerung.user.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// ==UserScript==
+// @name        tiktok rechtsklick steuerung
+// @version     1.0
+// @include     *tiktok.com*
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
+// @grant        none
+// @version      1
+// ==/UserScript==
+
+var styleSheet = `
+.tiktok-12azhi0-DivHeaderContainer {
+    z-index:1000000000000 !important;
+}
+`;
+
+(function () {
+  var s = document.createElement('style');
+  s.type = "text/css";
+  s.innerHTML = styleSheet;
+  (document.head || document.documentElement).appendChild(s);
+})();
+
+$(document).ready(function() {
+    let aktiv = false;
+
+    function videocheck() {
+        aktiv = true;
+        const timer = setInterval(function() {
+            const video = $("video");
+            if (video.length == 1) {
+                video.prop("controls", true);
+                video.css({
+                    "position" : "relative",
+                    "z-index" : 99999999
+                });
+                video.each(function() {
+                    const hier = $(this);
+                    const klasse = this.className;
+                    if (hier && klasse && typeof klasse === "string" && klasse.includes("-VideoBasic")) {
+                        console.log("gefunden1", hier);
+                        aktiv = false;
+                        clearInterval(timer);
+                    }
+                });
+                if (!aktiv) {
+                    return;
+                }
+                $("*").each(function() {
+                    const hier = $(this);
+                    const klasse = this.className;
+                    if (hier && klasse && typeof klasse === "string" && klasse.includes("-DivVideoControlContainer")) {
+                        console.log("gefunden2", hier);
+                        hier.fadeOut().css("pointer-events", "none");
+                        aktiv = false;
+                        clearInterval(timer);
+                    }
+                });
+            }
+        }, 100);
+    }
+
+    videocheck();
+
+    $(document).on("click", function() {
+        if (!aktiv) {
+            videocheck();
+        }
+    });
+});
